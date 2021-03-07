515c846b8c6d1342a29c739ce010c92a64a97e53
Cole Bemis <colebemis@github.com>
2021-03-07 11:35:23 -0800

    Usee css function in global styles

--- lib/src/ThemeProvider.tsx
+++ lib/src/ThemeProvider.tsx
@@ -2,7 +2,7 @@ import {
   ThemeProvider as ScThemeProvider,
   createGlobalStyle,
 } from 'styled-components';
-// import css from '@styled-system/css';
+import css from '@styled-system/css';
 import React from 'react';
 import { theme as defaultTheme } from './theme';
 
@@ -11,38 +11,21 @@ type ThemeProviderProps = {
   children?: React.ReactNode;
 };
 
-const GlobalStyle = createGlobalStyle`
-  *, *::before, *::after {
-    box-sizing: inherit;
-  }
-
-  body {
-    margin: 0;
-    color: ${props =>
-      // @ts-ignore
-      props.theme.colors.text.primary};
-    background-color: ${props =>
-      // @ts-ignore
-      props.theme.colors.bg.canvas};
-    line-height: ${props =>
-      // @ts-ignore
-      props.theme.lineHeights.default};
-    box-sizing: border-box;
-  }
-`;
-/* ${css({
-  '*, *::before, *::after': {
-    boxSizing: 'inherit',
-  },
-  body: {
-    margin: 0,
-    color: 'text.primary',
-    bg: 'bg.canvas',
-    fontFamily: 'normal',
-    lineHeight: 'default',
-    boxSizing: 'border-box',
-  },
-})} */
+const GlobalStyle = createGlobalStyle(
+  css({
+    '*, *::before, *::after': {
+      boxSizing: 'inherit',
+    },
+    body: {
+      margin: 0,
+      color: 'text.primary',
+      bg: 'bg.canvas',
+      fontFamily: 'normal',
+      lineHeight: 'default',
+      boxSizing: 'border-box',
+    },
+  })
+);
 
 export function ThemeProvider({
   theme = defaultTheme,
