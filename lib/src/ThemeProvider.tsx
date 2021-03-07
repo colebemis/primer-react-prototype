d24f349ca05ee9b755b95307048cf67e7e763590
Cole Bemis <colebemis@github.com>
2021-03-07 11:18:41 -0800

    Update global style

--- lib/src/ThemeProvider.tsx
+++ lib/src/ThemeProvider.tsx
@@ -2,7 +2,7 @@ import {
   ThemeProvider as ScThemeProvider,
   createGlobalStyle,
 } from 'styled-components';
-import css from '@styled-system/css';
+// import css from '@styled-system/css';
 import React from 'react';
 import { theme as defaultTheme } from './theme';
 
@@ -12,20 +12,37 @@ type ThemeProviderProps = {
 };
 
 const GlobalStyle = createGlobalStyle`
-  ${css({
-    '*, *::before, *::after': {
-      boxSizing: 'inherit',
-    },
-    body: {
-      margin: 0,
-      color: 'text.primary',
-      bg: 'bg.canvas',
-      fontFamily: 'normal',
-      lineHeight: 'default',
-      boxSizing: 'border-box',
-    },
-  })}
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    color: ${props =>
+      // @ts-ignore
+      props.theme.colors.text.primary};
+    background-color: ${props =>
+      // @ts-ignore
+      props.theme.colors.bg.canvas};
+    line-height: ${props =>
+      // @ts-ignore
+      props.theme.lineHeights.default};
+    box-sizing: border-box;
+  }
 `;
+/* ${css({
+  '*, *::before, *::after': {
+    boxSizing: 'inherit',
+  },
+  body: {
+    margin: 0,
+    color: 'text.primary',
+    bg: 'bg.canvas',
+    fontFamily: 'normal',
+    lineHeight: 'default',
+    boxSizing: 'border-box',
+  },
+})} */
 
 export function ThemeProvider({
   theme = defaultTheme,
